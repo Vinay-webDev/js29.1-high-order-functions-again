f056d3cdf13678f6ac3380293607e1a3f0523112
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-05 22:50:31 +0530

    1. forEach( )

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// higher order functions are the functions which does atleast one of the following
+/* 1. takes one or more functions as a parameter or (arguments)
+   2. returns function as the result */
+// ***remember need to use {} while exporting functions or files which are not default ****
+import {users} from './users.js';
+
+// 1. our first higher order function ==>> forEach()
+
+users.forEach(user => {
+   console.log(user);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
